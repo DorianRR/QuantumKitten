b53ecb1dd798f4b480459fc9c4282de17dd0d8ec
Dorian Rosen <dorianrrosen@gmail.com>
2018-01-20 15:22:03 -0700

    update mostly working with the reset bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(5, 0, 0), ForceMode.Impulse);
+        //gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(5, 0, 0), ForceMode.Impulse);
 	}
 	
 	// Update is called once per frame
