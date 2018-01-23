2b246d2401a9542acd1e4742385b4a7403f172a8
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2018-01-23 09:59:13 -0700

    public interface for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    
+
+    public Vector3 initialForce = new Vector3(10,7,0);
     // Use this for initialization
 	void Start () {
-        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(10, 7, 0), ForceMode.Impulse);
+        gameObject.GetComponent<Rigidbody>().AddForce(initialForce, ForceMode.Impulse);
 	}
 	
 	// Update is called once per frame
