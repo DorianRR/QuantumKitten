fc4763c59f65d8beb9d4689ec7a1581c3ed5c3c0
Rob <robaer.eae@gmail.com>
2018-01-26 14:13:00 -0700

    made star spawn animation look cooler

--- Assets/rotator.cs
+++ Assets/rotator.cs
@@ -14,13 +14,13 @@ public class rotator : MonoBehaviour {
     void FixedUpdate () {
         transform.Rotate(Vector3.up * .25f);
 
-        if(scaleTemp.x < 7 && !scaled)
+        if(scaleTemp.x < 8 && !scaled)
         {
             scaleTemp.x += .75f;
             scaleTemp.y += .75f;
             scaleTemp.z += .75f;
         }
-        else if(scaleTemp.x > 9 && !scaled)
+        else if(!scaled)
         {
             scaled = true;
         }
@@ -29,10 +29,6 @@ public class rotator : MonoBehaviour {
             scaleTemp.x -= .75f;
             scaleTemp.y -= .75f;
             scaleTemp.z -= .75f;
-        }
-        else
-        {
-            Debug.Log("Done");
         }
 
         transform.localScale = scaleTemp;
