7f560fdf9957980dfddb114f3067c030a63a749a
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2018-03-13 20:53:24 -0600

    boop

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -19,20 +19,23 @@ public class CameraController : MonoBehaviour
         transform.position = (player.GetComponent<Rigidbody>().transform.position);
         transform.position = new Vector3(transform.position.x, transform.position.y, -22f);
 
-       offset = transform.position - player.transform.position;
+        offset = transform.position - player.transform.position;
+
 
     }
 
     void LateUpdate()
     {
-       transform.position = (player.GetComponent<Rigidbody>().transform.position + offset);
-        // CAMERA BOUNDS
+        transform.position = (player.GetComponent<Rigidbody>().transform.position + offset);
+        //CAMERA BOUNDS
         float temp = ((Mathf.Abs(player.GetComponent<Rigidbody>().velocity.magnitude) - 10f) / (25f - 10f)) * 20f;
         temp = Mathf.Clamp(temp, 10f, 25f);
-        gameObject.GetComponent<Camera>().orthographicSize = 
-            Mathf.Lerp(gameObject.GetComponent<Camera>().orthographicSize, temp, lerpRatio/5 * Time.deltaTime);
+        gameObject.GetComponent<Camera>().orthographicSize =
+            Mathf.Lerp(gameObject.GetComponent<Camera>().orthographicSize, temp, lerpRatio / 5 * Time.deltaTime);
         float tempOrthSize = gameObject.GetComponent<Camera>().orthographicSize;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, (((tempOrthSize-10))*2f)-250, (-(tempOrthSize - 10) * 2f) + 250), Mathf.Clamp(transform.position.y, ((tempOrthSize-10)-200), (200 - (tempOrthSize - 10))), -22);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, (((tempOrthSize - 10)) * 2f) - 300, (-(tempOrthSize - 10) * 2f) + 300), Mathf.Clamp(transform.position.y, ((tempOrthSize - 10) - 250), (250 - (tempOrthSize - 10))), -22);
+
+
     }
 
     public void CenterOnSpawnedGW(Vector3 location)
