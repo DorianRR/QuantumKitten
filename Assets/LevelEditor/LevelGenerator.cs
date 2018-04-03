72f6ae6cef4cd650bb579b0a5b06263216ec5a8f
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2018-04-03 11:35:22 -0600

    level editor implemented, needs testing

--- Assets/LevelEditor/LevelGenerator.cs
+++ Assets/LevelEditor/LevelGenerator.cs
@@ -5,22 +5,38 @@ public class LevelGenerator : MonoBehaviour
 {
 
 	public Texture2D map;
+    public GameObject clickWall;
+    public GameObject bgWall;
+	public ColorToPrefab Player;
+    public ColorToPrefab Asteroid;
+    public ColorToPrefab SpeedAsteroid;
+    public ColorToPrefab Bumper;
+    public ColorToPrefab SlowBumper;
+    public ColorToPrefab NoTouchZone;
+    public ColorToPrefab Wall;
+    public ColorToPrefab Portal;
+    public ColorToPrefab ExitHole;
+    private ColorToPrefab[] colorMappings;
 
-	public ColorToPrefab[] colorMappings;
-
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start () 
 	{
 		
 		GenerateLevel();
-
-
-	}
+        colorMappings[0] = Player;
+        colorMappings[1] = Asteroid;
+        colorMappings[2] = SpeedAsteroid;
+        colorMappings[3] = Bumper;
+        colorMappings[4] = SlowBumper;
+        colorMappings[5] = NoTouchZone;
+        colorMappings[6] = Wall;
+    }
 
 	void GenerateLevel ()
 	{
-
-		for (int x = 0; x < map.width; x++)
+        Instantiate(clickWall, new Vector3(100, 100, 0), Quaternion.Euler(0,-90,90), null);
+        Instantiate(bgWall, new Vector3(100, 100, 20), Quaternion.Euler(0, -90, 90), null);
+        for (int x = 0; x < map.width; x++)
 		{
 			for (int y = 0; y < map.height; y++)
 			{
@@ -41,6 +57,7 @@ public class LevelGenerator : MonoBehaviour
 			//The pixel is transparrent.  Let's ignor it!
 			return;
 		}
+
 
 		foreach (ColorToPrefab colorMapping in colorMappings)
 		{
