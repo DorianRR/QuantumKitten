0e4c2d5a8ab834796cc04349c2abe1179210c60a
jbishop-art <jonathan.bishop.art@gmail.com>
2018-04-05 13:30:48 -0600

    l;kjh

---
+++ Assets/Scripts/TrailSwitch_vfx.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailSwitch_vfx : MonoBehaviour
+{
+
+    private float speed;
+    public ParticleSystem rainbowTrail;
+    public ParticleSystem destTrail;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+       
+
+        
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        
+        if (GetComponent<Rigidbody>().velocity.magnitude > 45)
+        {
+            destTrail.Play();
+            rainbowTrail.Stop();
+        }
+        else if (GetComponent<Rigidbody>().velocity.magnitude < 45)
+        {
+            rainbowTrail.Play();
+            destTrail.Stop();
+        }
+
+        
+    }
+    
+}
+
