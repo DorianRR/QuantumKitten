3390cd89a247460b52168bbe5595a70094f87058
Dorian Rosen <dorianrrosen@gmail.com>
2018-04-10 17:26:43 -0600

    final merge

--- Assets/LevelEditor/LevelGenerator.cs
+++ Assets/LevelEditor/LevelGenerator.cs
@@ -1,56 +1,84 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour 
+public class LevelGenerator : MonoBehaviour
 {
 
-	public Texture2D map;
+    public Texture2D map;
+    public GameObject clickWall;
+    public GameObject bgWall;
+    public ColorToPrefab Player;
+    public ColorToPrefab Asteroid;
+    public ColorToPrefab SpeedAsteroid;
+    public ColorToPrefab Bumper;
+    public ColorToPrefab SlowBumper;
+    public ColorToPrefab NoTouchZone;
+    public ColorToPrefab Wall;
+    public ColorToPrefab Portal;
+    public ColorToPrefab ExitHole;
+    private ColorToPrefab[] colorMappings;
+    private Quaternion[] rotations;
+    public CameraController testCam;
 
-	public ColorToPrefab[] colorMappings;
+    // Use this for initialization
+    void Start()
+    {
+        testCam = GameObject.Find("Main Camera").GetComponent<CameraController>();
+        colorMappings = new ColorToPrefab[7];
+        rotations = new Quaternion[7];
+        for (int i = 0; i < 7; i++)
+            rotations[i] = Quaternion.identity;
+        colorMappings[0] = Player;
+        colorMappings[1] = Asteroid;
+        colorMappings[2] = SpeedAsteroid;
+        colorMappings[3] = Bumper;
+        colorMappings[4] = SlowBumper;
+        colorMappings[5] = NoTouchZone;
+        rotations[5] = Quaternion.Euler(-90, 0, 0);
+        colorMappings[6] = Wall;
+        Debug.Log(colorMappings[6].color);
+        GenerateLevel();
+    }
 
-	// Use this for initialization
-	void Start () 
-	{
-		
-		GenerateLevel();
+    void GenerateLevel()
+    {
+        Instantiate(clickWall, new Vector3(100, 100, 0), Quaternion.Euler(0, -90, 90), null);
+        Instantiate(bgWall, new Vector3(100, 100, 20), Quaternion.Euler(0, -90, 90), null);
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+            }
+        }
+
+    }
+
+    void GenerateTile(int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+        Debug.Log(pixelColor.GetHashCode());
+
+        if (pixelColor.a == 0)
+        {
+            return;
+        }
 
 
-	}
+        for (int i = 0; i < 7; i++)
+        {
+            if (colorMappings[i].color.Equals(pixelColor))
+            {
+                Debug.Log("equal color");
+                Vector2 position = new Vector2(x, y);
+                GameObject newObject = Instantiate(colorMappings[i].prefab, position, rotations[i], transform);
+                if (i == 0)
+                {
+                    newObject.transform.name = "Player";
+                    testCam.player = newObject;
+                }
+            }
+        }
+    }
 
-	void GenerateLevel ()
-	{
 
-		for (int x = 0; x < map.width; x++)
-		{
-			for (int y = 0; y < map.height; y++)
-			{
-				GenerateTile(x,y);
-			}
-		}
-
-	}
-
-	void GenerateTile (int x, int y)
-	{
-		Color pixelColor = map.GetPixel(x,y);
-
-        Debug.Log("TEST TEST");
-
-		if (pixelColor.a == 0)
-		{
-			//The pixel is transparrent.  Let's ignor it!
-			return;
-		}
-
-		foreach (ColorToPrefab colorMapping in colorMappings)
-		{
-			if (colorMapping.color.Equals(pixelColor))
-			{
-				Vector2 position = new Vector2(x,y);
-				Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
-			}
-		}
-	}
-	
-	
 }
