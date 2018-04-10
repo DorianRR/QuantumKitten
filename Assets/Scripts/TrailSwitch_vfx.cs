705a3ea6ace733e2083fc7b15d3c8c9ba7ea6624
jbishop-art <jonathan.bishop.art@gmail.com>
2018-04-10 16:24:17 -0600

    Cat trail done

--- Assets/Scripts/TrailSwitch_vfx.cs
+++ Assets/Scripts/TrailSwitch_vfx.cs
@@ -4,38 +4,42 @@ using UnityEngine;
 
 public class TrailSwitch_vfx : MonoBehaviour
 {
+    private float speed;  //Float var that allows easy input of velocity.
+    public ParticleSystem rainbowTrail;  //Public var to drop in vfx.
+    public ParticleSystem destTrail;  //Public var to drop in vfx.
+    public ParticleSystem preBurst; //Public var to drop in vfx.
+    public float breakSpeed = 0.0f;  //Enter the velocity at which the cat can break things.
+    public float topTranformSpeed = 0.0f;  //This is the top end of the velocity at which the lighting works.
+    public float bottomTransformSpeed = 0.0f;  //This is the bottom end of the velocity at  which the lighting works.
 
-    private float speed;
-    public ParticleSystem rainbowTrail;
-    public ParticleSystem destTrail;
-
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
        
-
-        
-        
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
-    {
-        
-        if (GetComponent<Rigidbody>().velocity.magnitude > 45)
+    {        
+        if (GetComponent<Rigidbody>().velocity.magnitude > breakSpeed)  //If speed is > 45, Play destTrail prefab & Stop rainbowTrail prefab.
         {
-            destTrail.Play();
-            rainbowTrail.Stop();
+            destTrail.Play();  //Play destTrail vfx.
+            rainbowTrail.Stop();  //Stop rainbowTrail vfx.
         }
-        else if (GetComponent<Rigidbody>().velocity.magnitude < 45)
+        else if (GetComponent<Rigidbody>().velocity.magnitude < breakSpeed)
         {
-            rainbowTrail.Play();
-            destTrail.Stop();
-        }
-
+            rainbowTrail.Play();  //Play rainbowTrail vfx.
+            destTrail.Stop();  //Stop destTrail vfx.            
+        }  
         
-    }
-    
+        if ((GetComponent<Rigidbody>().velocity.magnitude < topTranformSpeed) && (GetComponent<Rigidbody>().velocity.magnitude > bottomTransformSpeed))
+        {
+            preBurst.Play(); //Play the preBurst vfx.
+        }
+        else
+        {
+            preBurst.Stop();  //Stop the preBurst vfx.
+        }
+    }    
 }
 
